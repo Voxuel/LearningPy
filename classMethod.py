8f68993f6ee95e91b77da5df68085921ec675181
Voxuel <Leo.Fridh@hotmail.com>
2023-07-03 16:33:08 +0200

    fix

--- classMethod.py
+++ classMethod.py
@@ -6,14 +6,15 @@ class PlayerCharacter:
         self.age = age
 
     def shout(self):
+        return None
 
-        @classmethod
-        def adding_things(cls, num1, num2):
-            return cls('Joe', num1 + num2)
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return cls('Joe', num1 + num2)
 
     @staticmethod
     def adding_things2(num1, num2):
         return num1 + num2
 
 
-print(PlayerCharacter.adding_things2(2, 3))
+print(PlayerCharacter.adding_things(2, 3))
